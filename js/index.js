e5027bf80adc2a12ccfe8cf176a19a0d6245ac43
mo <1430337380@qq.com>
2019-04-04 08:36:41 +0800

    移动端品优购1.0

---
+++ js/index.js
@@ -0,0 +1,34 @@
+$(function () {
+    $.ajax({
+        type: 'get',
+        url: 'http://157.122.54.189:9094/api/public/v1/home/swiperdata',
+        dataType: 'json',
+        success: function (result) {
+            console.log(result);
+            var html = template('pyg_bannerTemp', result)
+            $('.mui-slider-group').html(html)
+
+            //获得slider插件对象
+            mui('.mui-slider').slider({
+                interval: 2000      //自动轮播周期，若为0则不自动播放，默认为0；
+            })
+        }
+    })
+    $.ajax({
+        type:'get',
+        url:'http://157.122.54.189:9094/api/public/v1/home/goodslist',
+        dataType:'json',
+        success:function(result){
+            console.log(result);
+            var html=template('pyg_commodity',result)
+            console.log(html);
+            
+            $('.commodity').html(html)
+            
+        }
+    })
+
+ 
+
+
+})
